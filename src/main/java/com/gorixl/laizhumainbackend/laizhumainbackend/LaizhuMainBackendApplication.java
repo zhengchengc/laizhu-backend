8e5ae23b8ae1e8b2cf80af71376556cd351f7730
zhengchengc <z@olichid.com>
2020-04-06 11:34:10 +1000

    initialize the project

---
+++ src/main/java/com/gorixl/laizhumainbackend/laizhumainbackend/LaizhuMainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gorixl.laizhumainbackend.laizhumainbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaizhuMainBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaizhuMainBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gorixl/laizhumainbackend/laizhumainbackend/LaizhuMainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorixl.laizhumainbackend.laizhumainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaizhuMainBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
